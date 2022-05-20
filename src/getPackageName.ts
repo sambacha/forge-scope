739e971bf9aeb8b0c1eef7c6b437c06af8f2fd52
sam bacha <sam@manifoldfinance.com>
2022-05-20 13:33:37 -0700

    Create getPackageName.ts

---
+++ src/getPackageName.ts
@@ -0,0 +1,48 @@
+// taken from create-react-app
+// Extract package name from tarball url or path.
+function getPackageName(installPackage) {
+  if (installPackage.indexOf('.tgz') > -1) {
+    return getTemporaryDirectory()
+      .then(obj => {
+        let stream;
+        if (/^http/.test(installPackage)) {
+          stream = hyperquest(installPackage);
+        } else {
+          stream = fs.createReadStream(installPackage);
+        }
+        return extractStream(stream, obj.tmpdir).then(() => obj);
+      })
+      .then(obj => {
+        const packageName = require(path.join(obj.tmpdir, 'package.json')).name;
+        obj.cleanup();
+        return packageName;
+      })
+      .catch(err => {
+        // The package name could be with or without semver version, e.g. react-scripts-0.2.0-alpha.1.tgz
+        // However, this function returns package name only without semver version.
+        console.log(
+          `Could not extract the package name from the archive: ${err.message}`
+        );
+        const assumedProjectName = installPackage.match(
+          /^.+\/(.+?)(?:-\d+.+)?\.tgz$/
+        )[1];
+        console.log(
+          `Based on the filename, assuming it is "${chalk.cyan(
+            assumedProjectName
+          )}"`
+        );
+        return Promise.resolve(assumedProjectName);
+      });
+  } else if (installPackage.indexOf('git+') === 0) {
+    // Pull package name out of git urls e.g:
+    // git+https://github.com/mycompany/react-scripts.git
+    // git+ssh://github.com/mycompany/react-scripts.git#v1.2.3
+    return Promise.resolve(installPackage.match(/([^/]+)\.git(#.*)?$/)[1]);
+  } else if (installPackage.match(/.+@/)) {
+    // Do not match @scope/ when stripping off @version or @tag
+    return Promise.resolve(
+      installPackage.charAt(0) + installPackage.substr(1).split('@')[0]
+    );
+  }
+  return Promise.resolve(installPackage);
+}
